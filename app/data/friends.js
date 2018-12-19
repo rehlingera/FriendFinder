2074e3568a5200cd9ba58ecf3d17fde2879d003a
Relliner <41902228+rehlingera@users.noreply.github.com>
2018-12-18 22:12:32 -0600

    basic survery page created

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friends = [
+    {
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },{
+        "name":"Steve",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            3,
+            2,
+            5,
+            1,
+            2,
+            2,
+            5,
+            3,
+            3,
+            2
+        ]
+    }
+];
+
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,7 @@
+app.get("/api/friends", function(req,res) {
+    return(res.json(sitting));
+});
+
+app.post("/api/friends", function(req,res) {
+    return(res.json(sitting));
+});
