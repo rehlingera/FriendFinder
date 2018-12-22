89648da6bf7851c9442aa272b95d7cec26ab9632
Relliner <41902228+rehlingera@users.noreply.github.com>
2018-12-22 17:27:56 -0600

    everything seems to be working well

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,24 +6,29 @@ module.exports = function(app){
     });
 
     app.post("/api/friends", function(req,res) {
+        // potential new friend request
         var newFriend = req.body;
         var match = friends[0];
         var diffs = [];
+        // Search all friends
         for(var i=0;i<friends.length;i++){
+            // Declare new difference for this friend
             var totalDiff = 0;
             for (var x=0;x<friends[i].scores.length;x++){
                 var compare = Math.abs(newFriend.scores[x] - friends[i].scores[x]);
+                // Calculate difference between friend and I
                 totalDiff += compare;
             };
+
+
+            // Add to difference mapped array
             diffs.push(totalDiff);
         };
         var least = Math.min(...diffs);
         for (var i=0;i<diffs.length;i++){
             if(diffs[i]===least){
                 match=friends[i];
-                return(match);
             };
-            return(match);
         };
 
         friends.push(newFriend);
